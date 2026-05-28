075b8e29b43840b26ba7e5204f724531e0c55a19
PoornaChaitanya <poornachaitanya4259@gmail.com>
2026-05-28 21:59:13 +0530

    Add analysisController with basic CRUD operations for SEO analysis

---
+++ server/controllers/analysisController.js
@@ -0,0 +1,11 @@
+// Analyze a URL
+export const analyzeUrl = async (req, res) => {};
+
+// Get analysis by ID
+export const getAnalysis = async (req, res) => {};
+
+// Get all analyses for user
+export const getAnaylses = async (req, res) => {};
+
+// Delete analysis
+export const deleteAnaylsis = async (req, res) => {};
